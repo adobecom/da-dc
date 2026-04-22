d150042a34a3049eb8a1faa345f82616ba419980
Jingle Huang <32369333+JingleH@users.noreply.github.com>
2026-04-22 15:19:17 -0500

    MWPW-185573 remove unused references to dc widget (#87)
    * incorporated pr 24
    
    * rm deprecated ut

--- edgeworkers/Acrobat_DC_web_prod/main.js
+++ edgeworkers/Acrobat_DC_web_prod/main.js
@@ -6,8 +6,6 @@ import { createResponse } from 'create-response';
 import { httpRequest } from 'http-request';
 //import { logger } from 'log';
 import { EdgeKV } from './edgekv.js';
-import localeMap from './utils/locales.js';
-import verbMap from './utils/verbs.js';
 import contentSecurityPolicy from './utils/csp/index.js';
 
 export async function responseProvider(request) {
@@ -16,9 +14,7 @@ export async function responseProvider(request) {
   }
   const path = request.path.split('/');
   const first = path[1];
-  const locale = localeMap[first];
   const last = path.splice(-1)[0].split('.')[0];
-  const verb = verbMap[last] || last;
   const origin = `${request.scheme}://${request.host}`;
   const isProd = request.host === 'www.adobe.com';
   const rewriter = new HtmlRewritingStream();
@@ -38,16 +34,10 @@ export async function responseProvider(request) {
       throw err;
     }
 
-    // Make preliminary pass through the content to capture version metadata
+    // Make preliminary pass through the content to capture metadata
     const firstPassRewriter = new HtmlRewritingStream();
-    let version, widgetVersion, mobileWidget, unityWorkflow;
+    let mobileWidget, unityWorkflow;
     const prefix = isProd ? '' : 'stg-';
-    firstPassRewriter.onElement(`meta[name="${prefix}dc-widget-version"]`, el => {
-      widgetVersion = el.getAttribute('content');
-    });
-    firstPassRewriter.onElement(`meta[name="${prefix}dc-generate-cache-version"]`, el => {
-      version = el.getAttribute('content');
-    });
     firstPassRewriter.onElement('meta[name="mobile-widget"]', el => {
       mobileWidget = el.getAttribute('content');
     });
@@ -87,7 +77,7 @@ export async function responseProvider(request) {
       delete responseHeaders[prop];
     }
 
-    return [responseStream, responseHeaders, version, widgetVersion, mobileWidget, unityWorkflow, studyMarquee];
+    return [responseStream, responseHeaders, mobileWidget, unityWorkflow, studyMarquee];
   };
 
   const fetchResource = async path => {
@@ -96,31 +86,6 @@ export async function responseProvider(request) {
       return response.text();
     }
     throw new Error(`Failed to fetch resource: ${path} status: ${response.status}`);
-  };
-
-  const fetchFrictionlessPageAndInlineSnippet = async () => {
-    const [responseStream, responseHeaders, version, widgetVersion, mobileWidget, unityWorkflow, studyMarquee] = await fetchFrictionlessPage();
-
-    if (!verb || !locale || !version || !widgetVersion) {
-      throw new Error('Missing metadata');
-    }
-
-    if (!(mobileWidget && request.device.isMobile) && !unityWorkflow) {
-      const snippet =
-        await fetchResource(`/dc/dc-generate-cache/dc-hosted-${version}/${verb}-${locale}.html`);
-      const snippetHead = snippet.substring(snippet.indexOf('<head>')+6, snippet.indexOf('</head>'));
-      const snippetBody = snippet.substring(snippet.indexOf('<body>')+6, snippet.indexOf('</body>'));
-
-      rewriter.onElement('head', el => {
-        el.append(snippetHead);
-      });
-      rewriter.onElement('div.dc-converter-widget', el => {
-        el.append(`<section id="edge-snippet">${snippetBody}</section>`);
-      });
-    }
-    const dcCoreVersion = widgetVersion.split("_")[0];
-
-    return [responseStream, responseHeaders, dcCoreVersion, mobileWidget, unityWorkflow, studyMarquee];
   };
 
   const scriptHashes = [];
@@ -204,7 +169,7 @@ export async function responseProvider(request) {
 
   try {
     const [
-      [responseStream, responseHeaders, dcCoreVersion, mobileWidget, unityWorkflow, studyMarquee],
+      [responseStream, responseHeaders, mobileWidget, unityWorkflow, studyMarquee],
       scripts,
       dcConverter,
       dcStyles,
@@ -212,7 +177,7 @@ export async function responseProvider(request) {
       verbWidgetStyles,
       studyMarqueeStyles
     ] = await Promise.all([
-      fetchFrictionlessPageAndInlineSnippet(),
+      fetchFrictionlessPage(),
       fetchResource('/acrobat/scripts/scripts.js'),
       fetchResource('/acrobat/blocks/dc-converter-widget/dc-converter-widget.js'),
       fetchResource('/acrobat/styles/styles.css'),
@@ -226,7 +191,6 @@ export async function responseProvider(request) {
 
     const csp = contentSecurityPolicy(isProd, scriptHashes);
     const acrobat = isProd ? 'https://acrobat.adobe.com' : 'https://stage.acrobat.adobe.com';
-    const pdfnow = isProd ? 'https://pdfnow.adobe.io' : 'https://pdfnow-stage.adobe.io';
     const adobeid = isProd ? 'https://adobeid-na1.services.adobe.com' : 'https://adobeid-na1-stg1.services.adobe.com';
 
     let headerLink = [
@@ -255,13 +219,6 @@ export async function responseProvider(request) {
           `</acrobat/blocks/verb-widget/verb-widget.css>;rel="preload";as="style"`,
         ];
       }
-    } else if (!(mobileWidget && request.device.isMobile)) {
-      headerLink = [...headerLink,
-        `<${acrobat}>;rel="preconnect"`,
-        `<${pdfnow}>;rel="preconnect"`,
-        `<${acrobat}/dc-core/${dcCoreVersion}/dc-core.js>;rel="preload";as="script"`,
-        `<${acrobat}/dc-core/${dcCoreVersion}/dc-core.css>;rel="preload";as="style"`,
-      ];
     }
     headerLink = headerLink.join();
 
