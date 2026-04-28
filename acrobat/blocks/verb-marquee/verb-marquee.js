246cc5dfac58d5dfcf973b47ee56aa582b5e3f93
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2026-04-27 18:24:00 -0700

    [MWPW-193863]Add solo verb cta logic to verb-marquee for redesign test (#98)
    * add solo verb cta
    
    * make sure duplicate analytics events don't trigger
    
    ---------
    
    Co-authored-by: Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>

--- acrobat/blocks/verb-marquee/verb-marquee.js
+++ acrobat/blocks/verb-marquee/verb-marquee.js
@@ -502,6 +502,7 @@ export default async function init(element) {
     }
   }
 
+  let soloClicked = false;
   let fileInput = null;
   if (useFileUpload) {
     fileInput = createTag('input', {
@@ -737,6 +738,10 @@ export default async function init(element) {
       }
     });
     fileInput.addEventListener('click', () => {
+      if (soloClicked) {
+        soloClicked = false;
+        return;
+      }
       [
         'filepicker:shown',
         'dropzone:choose-file-clicked',
@@ -763,6 +768,37 @@ export default async function init(element) {
     errorStateText.textContent = '';
     clearSrAlert();
   });
+  function soloUpload() {
+    if (!useFileUpload || !fileInput || !ctaButton) return;
+    const uploadLinks = document.querySelectorAll('a[href*="#upload"]');
+    uploadLinks.forEach((link) => {
+      const labelElement = createTag('label', {
+        for: 'file-upload',
+        class: 'verb-marquee-cta verb-marquee-cta-solo',
+        tabindex: 0,
+        'daa-ll': ctaButton.textContent,
+        'aria-label': ctaButton.textContent,
+      });
+      labelElement.innerHTML = ctaButton.innerHTML;
+      const wrapper = link.closest('div');
+      if (!wrapper) return;
+      wrapper.append(labelElement);
+      link.remove();
+      labelElement.addEventListener('click', (data) => {
+        soloClicked = true;
+        [
+          'filepicker:shown',
+          'cta:choose-file-clicked',
+          'files-selected',
+          'entry:clicked',
+          'discover:clicked',
+        ].forEach((analyticsEvent) => {
+          window.analytics.verbAnalytics(analyticsEvent, VERB, { ...data, userAttempts });
+        });
+      });
+    });
+  }
+  runWhenDocumentIsReady(soloUpload);
   element.addEventListener('unity:track-analytics', (e) => {
     const cookieExp = new Date(Date.now() + 30 * 60 * 1000).toUTCString();
     const { event, data } = e.detail || {};
