81352d65e9999cc3ebf7bce2d840b4f9ea2a2df9
Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>
2026-05-28 09:24:10 -0700

    update dummy prefetch url

--- acrobat/blocks/verb-marquee/verb-marquee.js
+++ acrobat/blocks/verb-marquee/verb-marquee.js
@@ -191,7 +191,7 @@ function buildWordToPdfEarlyPrefetchUrl() {
   const [languageCode, languageRegion] = locale.split('-');
   const domain = DC_ENV === 'prod' ? 'acrobat.adobe.com' : 'stage.acrobat.adobe.com';
   const dummyAssets = 'urn%3Aaaid%3Asc%3AUS%3A1111111%7CSample%20word%20file_WordtoPDF.docx%7C386919%7Capplication%2Fvnd.openxmlformats-officedocument.wordprocessingml.document';
-  return `https://${domain}/${languageRegion}/${languageCode}/word-to-pdf?x_api_client_id=unity&x_api_client_location=word-to-pdf&user=frictionless_return_user&attempts=2%2B#assets=${dummyAssets}`;
+  return `https://${domain}/${languageRegion}/${languageCode}/word-to-pdf/av?x_api_client_id=unity&x_api_client_location=word-to-pdf&user=frictionless_return_user&attempts=2%2B#assets=${dummyAssets}`;
 }
 
 function handleExit(event, verb, userObj, unloadFlag, workflowStep) {
