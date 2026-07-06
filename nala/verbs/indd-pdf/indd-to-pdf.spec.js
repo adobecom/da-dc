27c5c7a701e9980ee0187559df38372de00f89a3
Predrag Markovic <137193827+markovicpredrag1209@users.noreply.github.com>
2026-07-06 22:36:36 +0200

    MWPW-199081: adding tests for new compress and image verbs (#138)
    * adding tests for new compress and image verbs
    
    * remove caas check
    
    ---------
    
    Co-authored-by: Predrag Markovic <cod14230@adobe.com>

---
+++ nala/verbs/indd-pdf/indd-to-pdf.spec.js
@@ -0,0 +1,16 @@
+module.exports = {
+  FeatureName: 'INDD to PDF',
+  features: [
+    {
+      tcid: '0',
+      name: '@indd-to-pdf',
+      path: '/acrobat/online/indd-to-pdf',
+      data: {
+        verbTitle: 'Adobe Acrobat',
+        verbHeading: 'INDD to PDF',
+        verbCopy: 'Drag and drop an InDesign file to use our INDD to PDF converter. Free to use, no sign-up to try.',
+      },
+      tags: '@indd-to-pdf @smoke @regression @unity',
+    },
+  ],
+};
