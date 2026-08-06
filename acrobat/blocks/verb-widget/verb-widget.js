3a23859e2a016037c8e4c53c4af879edfdf24f04
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2026-08-06 14:12:31 -0700

    [MWPW-202751]Make verb reskin independent of code update (#156)
    * update for future reskin verbs
    
    * cleanup
    
    ---------
    
    Co-authored-by: Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>

--- acrobat/blocks/verb-widget/verb-widget.js
+++ acrobat/blocks/verb-widget/verb-widget.js
@@ -395,6 +395,16 @@ async function createSvgElement(iconName) {
   return svgElement.cloneNode(true);
 }
 
+function getAuthoredVerbIcon(element) {
+  const img = element.querySelector('img');
+  if (img) return img;
+  const svgLink = element.querySelector('a[href$=".svg"]');
+  if (svgLink) {
+    return createTag('img', { src: svgLink.getAttribute('href'), alt: '' });
+  }
+  return null;
+}
+
 // Initialize analytics functions as no-ops until loaded
 window.analytics = {
   verbAnalytics: () => {},
@@ -540,6 +550,7 @@ export default async function init(element) {
     widgetSubHeading = children[1].textContent;
     widgetMobSubHeading = children[2].textContent;
   }
+  const authoredIcon = getAuthoredVerbIcon(element);
   let noOfFiles = null;
   let openFilePicker = true;
   const userAttempts = getVerbKey(`${VERB}_attempts`);
@@ -593,11 +604,12 @@ export default async function init(element) {
     ...(LIMITS[VERB]?.multipleFiles && { multiple: '' }),
   });
   const widgetImage = createTag('div', { class: 'verb-image' });
-  const verbIconName = `${VERB}`;
-  const verbImageSvg = await createSvgElement(verbIconName);
+  const verbImageSvg = authoredIcon || await createSvgElement(`${VERB}`);
   if (verbImageSvg) {
     verbImageSvg.classList.add('icon-verb-image');
-    verbImageSvg.setAttribute('alt', window.mph[`verb-widget-${VERB}-alt`] || VERB);
+    if (!verbImageSvg.getAttribute('alt')) {
+      verbImageSvg.setAttribute('alt', window.mph[`verb-widget-${VERB}-alt`] || VERB);
+    }
     widgetImage.appendChild(verbImageSvg);
   }
 
