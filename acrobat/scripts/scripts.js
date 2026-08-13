c1fec08c6483c9157ab73885c3686735ceac2c83
Ketaki Deshwandikar <ketaki@youremail.com>
2026-08-13 11:13:31 +0530

    minor fix

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -530,11 +530,13 @@ function getMasLocale(miloLocale) {
 function preloadMasFragment(a, config) {
   let url;
   try {
+    // eslint-disable-next-line compat/compat
     url = new URL(a.href);
   } catch {
     return;
   }
   if (url.hostname !== 'mas.adobe.com' || !url.pathname.endsWith('/studio.html')) return;
+  // eslint-disable-next-line compat/compat
   const params = new URLSearchParams(url.hash.replace(/^#/, ''));
   const fragment = params.get('fragment') || params.get('query');
   if (!fragment) return;
