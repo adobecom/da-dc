328955171041094e770fcabe3bdb98ae911e95cc
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2026-09-02 07:11:06 -0700

    Revert "remove 3 second wait from verb redesign blocks for analytics (#178)" (#187)
    This reverts commit db854f77bb3b927a01c14704fa12e9c07eea2cc6.

--- acrobat/blocks/verb-redesign-test/verb-redesign-test.js
+++ acrobat/blocks/verb-redesign-test/verb-redesign-test.js
@@ -583,7 +583,7 @@ export default async function init(element) {
 
   function handleUploadedEvent(data, attempts, cookieExp, canSendDataToSplunk) {
     exitFlag = true;
-    if (LIMITS[VERB]?.noRedirectTimeout ?? true) {
+    if (LIMITS[VERB]?.noRedirectTimeout) {
       window.dispatchEvent(redirectReady);
     } else {
       setTimeout(() => {
